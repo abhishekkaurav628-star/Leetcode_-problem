6ae617889d990495b1dc9ccf4b54d2a1a887ebab
Student Name <kauravabhishek628@gmail.com>
2026-06-20 01:04:42 +0530

    Add reverse integer solution

---
+++ Reverseint.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Reverseint {
+    public static void main(String[] args) {
+        int n = 123;
+        int rev =0; 
+        
+
+        while(n!=0){
+         int digit = n%10;
+
+         rev = rev* 10+digit;
+
+        n = n/10;
+
+
+        }
+        System.out.println("Reverse integer value:"+rev);
+}
+}
